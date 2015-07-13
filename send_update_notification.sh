a3bda30f8f4e3ece6d0ce5c718a6060632803cb9
Christian Franke <christian.franke@immobilienscout24.de>
2015-07-13 16:56:08 +0200

    VAL-2634: add param checks

--- send_update_notification.sh
+++ send_update_notification.sh
@@ -2,9 +2,11 @@
 stack=$1
 version=$2
 
+if [[ -z $1 || -z $2 ]]
+then
+	echo "usage: $0 <stackName> <version>"
+	exit 1
+fi
+
 aws sns publish --topic-arn arn:aws:sns:eu-west-1:744969810879:stackUpdateMessages \
-    --message '{ "stackName": "perf-6", "region": "eu-central-1", "params": { "dockerImageVersion": "30"}}'
-
-# { "stackName": "perf-6", "region": "eu-central-1", "params": { "dockerImageVersion": "27"}}
-
-# {"default": "{ \"stackName\": \"perf-6\", \"region\": \"eu-central-1\", \"params\": { \"dockerImageVersion\": \"28\"}}","lambda": "{ \"stackName\": \"perf-6\", \"region\": \"eu-central-1\", \"params\": { \"dockerImageVersion\": \"28\"}}"}
+    --message "{ \"stackName\": \"perf-6\", \"region\": \"eu-central-1\", \"params\": { \"dockerImageVersion\": \"$version\"}}"
