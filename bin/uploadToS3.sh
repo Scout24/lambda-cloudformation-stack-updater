18e2a033a2091b5e59df1778b8779c550e0684bb
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2015-10-14 15:31:40 +0200

    fix acl setting and do not rely on default region

--- bin/uploadToS3.sh
+++ bin/uploadToS3.sh
@@ -6,4 +6,4 @@ if [ -z "$version" ]; then
     exit 1
 fi
 
-aws s3 cp $(find target -name lambda-cloudFormation-stack-updater-$version.jar) s3://de.is24.val.update-stack-function
+aws s3 cp --acl public-read --region eu-west-1 $(find target -name lambda-cloudFormation-stack-updater-$version.jar) s3://de.is24.val.update-stack-function
