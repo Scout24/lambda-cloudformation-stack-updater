543491100035baf913507da28b00d3a86e4f82cf
Oliver Schmitz-Hennemann <oli99sc@gmail.com>
2015-10-14 15:33:33 +0200

    make upload script more talkative

--- bin/uploadToS3.sh
+++ bin/uploadToS3.sh
@@ -5,5 +5,6 @@ if [ -z "$version" ]; then
     echo "usage: $0 version"
     exit 1
 fi
-
+echo "about to upload jar to s3 using the following command:"
+echo "aws s3 cp --acl public-read --region eu-west-1 $(find target -name lambda-cloudFormation-stack-updater-$version.jar) s3://de.is24.val.update-stack-function"
 aws s3 cp --acl public-read --region eu-west-1 $(find target -name lambda-cloudFormation-stack-updater-$version.jar) s3://de.is24.val.update-stack-function
